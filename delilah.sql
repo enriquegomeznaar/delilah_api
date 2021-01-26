f56cb25b4916191a7838c4613d0563c888a53177
enriquegomeznaar <enriquegomeznaar@gmail.com>
2021-01-26 19:00:26 -0300

    agregado queries de la DB

---
+++ delilah.sql
@@ -0,0 +1,67 @@
+-- --------------------------------------------------------
+-- Host:                         localhost
+-- Versión del servidor:         10.1.35-MariaDB - mariadb.org binary distribution
+-- SO del servidor:              Win32
+-- HeidiSQL Versión:             9.5.0.5196
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+-- Volcando estructura de base de datos para delilah
+CREATE DATABASE IF NOT EXISTS `delilah` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `delilah`;
+
+-- Volcando estructura para tabla delilah.pedidos
+CREATE TABLE IF NOT EXISTS `pedidos` (
+  `idpedido` int(11) NOT NULL AUTO_INCREMENT,
+  `iduser` int(11) NOT NULL,
+  `metodo` varchar(50) NOT NULL,
+  `hora` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `estado` varchar(50) NOT NULL DEFAULT '1',
+  `pago` int(11) NOT NULL,
+  PRIMARY KEY (`idpedido`)
+) ENGINE=InnoDB AUTO_INCREMENT=110 DEFAULT CHARSET=latin1;
+
+-- La exportación de datos fue deseleccionada.
+-- Volcando estructura para tabla delilah.pedidos_list
+CREATE TABLE IF NOT EXISTS `pedidos_list` (
+  `idlist` int(11) NOT NULL AUTO_INCREMENT,
+  `idproducto` int(11) NOT NULL,
+  `idpedido` int(11) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  PRIMARY KEY (`idlist`)
+) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=latin1;
+
+-- La exportación de datos fue deseleccionada.
+-- Volcando estructura para tabla delilah.productos
+CREATE TABLE IF NOT EXISTS `productos` (
+  `idProducto` int(11) NOT NULL AUTO_INCREMENT,
+  `precio` int(11) NOT NULL,
+  `nombre_producto` varchar(50) NOT NULL,
+  `descripcion` varchar(50) NOT NULL,
+  PRIMARY KEY (`idProducto`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
+
+-- La exportación de datos fue deseleccionada.
+-- Volcando estructura para tabla delilah.usuarios
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `iduser` int(11) NOT NULL AUTO_INCREMENT,
+  `usuario` varchar(50) NOT NULL,
+  `direccion` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `admin` int(11) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `telefono` int(11) NOT NULL,
+  PRIMARY KEY (`iduser`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+
+-- La exportación de datos fue deseleccionada.
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
